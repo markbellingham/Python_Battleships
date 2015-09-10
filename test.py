2ec65facb37358bbb4b5f913e1778660c6880ab8
Mark Bellingham <14032098@stu.mmu.ac.uk>
2015-09-10 16:15:37 +0100

    fixed list.append issue

--- test.py
+++ test.py
@@ -12,20 +12,13 @@ def random_col(board):
     return randint(0, len(board[0]) - 1)
 
 ships = []
-ship = []
 
 while len(ships) < 3:
-    #ship = []
-    ship_row = random_row(board)
-    ship_col = random_col(board)
-    ship.append(ship_row)
-    ship.append(ship_col)
+    ship = []
+    ship.append(random_row(board))
+    ship.append(random_col(board))
     if ship not in ships:
         ships.append(ship)
 
 for i in range(len(ships)):
-    ship = ships[i]
-    for j in ship:
-        ship_row = j[0]
-        ship_col = j[1]
-        print "ship" + i + ":",ship_row, ship_col
+    print "ship " + str(i) + ":",ships[i]

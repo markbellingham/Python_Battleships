4b4e87444c740a1856e9dc74c3ff250137aff8c6
Mark Bellingham <14032098@stu.mmu.ac.uk>
2015-09-22 15:23:22 +0100

    some progress

--- battleships.py
+++ battleships.py
@@ -2,67 +2,25 @@ from random import randint
 import subprocess as sp
 
 board = []
-message = ""
 turn = 1
 ships = []
 ships_down = 0
-game_length = 10
+game_length = 6
+
+
 
 for x in range(5):
     board.append(["O"] * 5)
 
-def print_board(board):
-    global message
-    global turn
-    global game_length
-    tmp = sp.call('clear',shell=True)
-    print "Let's play Battleship!"
-    print
-    for row in board:
-        print " ".join(row)
-    print 
-    print message
-    message = ""
-    # Print (turn + 1) here unless game is over
-    if turn < game_length:
-        print "Turn:", turn
 
-print_board(board)
 
 def random_row(board):
-    return randint(0, len(board) - 1)
+    return randint(1, len(board))
 
 def random_col(board):
-    return randint(0, len(board[0]) - 1)
+    return randint(1, len(board[0]))
 
-def ship_attack(guess):
-    global ships_down
-    global message
-    global game_length
-    for ship in ships:
-        if guess == ship:
-            print "Congratulations! You sunk battleship", ship
-            board[guess_row][guess_col] = "X"
-            ships_down += 1
-        else:
-            if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-                message = "Oops, that's not even in the ocean."
-                break
-            elif(board[guess_row][guess_col] == "^"):
-                message = "You guessed that one already."
-                if turn == game_length - 1:
-                    message = "Game Over"
-                break
-            else:
-                message = "You missed my battleship!"
-                board[guess_row][guess_col] = "^"
-                #guess_row = ""
-                #guess_col = ""
-                if turn == game_length - 1:
-                    message = "Game Over"
-                break
-        if ships_down == len(ships):
-            break
+
 
 while len(ships) < 3:
     ship = []
@@ -71,26 +29,92 @@ while len(ships) < 3:
     if ship not in ships:
         ships.append(ship)
 
-for turn in range(1,game_length):
+
+
+def print_board(board):
+    global turn
+    global game_length
+    global message
+    tmp = sp.call('clear',shell=True)
+    print "Let's play Battleship!"
+    print
+    for row in board:
+        print " ".join(row)
+    print
+    for i in range(len(ships)):
+        print "ship " + str(i) + ":",ships[i]
+    print
+    try:
+        print "Guess is: ", guess
+    except NameError:
+        print
+    print
+    try:
+        print message
+    except NameError:
+        print NameError
+    if turn < game_length:
+        print "Turn:", turn
+
+
+
+def make_guess():
     guess = []
     while True:
         try:
-            guess_row = int(raw_input("Guess Row: ")) - 1
+            guess_row = int(raw_input("Guess Row: "))
             break
         except ValueError:
             print "That input is not valid"
     while True:
         try:
-            guess_col = int(raw_input("Guess Col: ")) - 1
+            guess_col = int(raw_input("Guess Col: "))
             break
         except ValueError:
             print "That input is not valid"
-    guess.append(guess_row)
-    guess.append(guess_col)
-    ship_attack(guess)
-    
-    turn = turn + 1
+    guess = [guess_row, guess_col]
+    return guess
+
+
+
+def ship_attack(guess):
+    global ships_down
+    global game_length
+    guess_row = guess[0]
+    guess_col = guess[1]
+    for ship in ships:
+        if guess == ship:
+            message = "Congratulations! You sunk battleship", ship
+            board[guess_row - 1][guess_col - 1] = "X"
+            ships_down += 1
+            return message
+        else:
+            if (guess_row < 1 or guess_row > 5) or (guess_col < 1 or guess_col > 5):
+                message = "Oops, that's not even in the ocean."
+                return message
+            elif(board[guess_row - 1][guess_col - 1] == "^"):
+                message = "You guessed that one already."
+                if turn == game_length:
+                    message = "Game Over"
+                    return message
+            else:
+                message = "You missed my battleship!"
+                board[guess_row - 1][guess_col - 1] = "^"
+                if turn == game_length:
+                    message = "Game Over"
+                    return message
+        if ships_down == len(ships):
+            message = "Congratulations! You sunk all my battleships!"
+            return message
+    return message
+
+
+
+for turn in range(1,game_length):
     print_board(board)
+    guess = make_guess()
+    ship_attack(guess)
+    turn += 1
 
 
 # Extra Credit
